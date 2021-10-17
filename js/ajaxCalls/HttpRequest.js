d81841e790f21d23cf51714735274fd216d31615
ajaybarath <ajaybarath3@gmail.com>
2021-10-17 20:28:15 +0530

    Refractored: http request with promise class

--- js/ajaxCalls/HttpRequest.js
+++ js/ajaxCalls/HttpRequest.js
@@ -1,59 +1,33 @@
-let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
-
-function makeAjaxCall(methodType, url, callback, async = true, data = null) {
-    let xhr = new XMLHttpRequest();
-    xhr.onreadystatechange = function() {
-        console.log("State changed called . Ready state: " + xhr.readyState + " State: " + xhr.status)
-        if (xhr.readyState === 4) {
-            if (xhr.status == 200 || xhr.status == 201) {
-                callback(xhr.responseText);
-            }
-            else {
-                console.log("Handle 400 client error or 500 server error")
+function makePromiseCall(methodType, url, async = true, data = null) {
+    return new Promise(function (resolve, reject) {
+        let xhr = new XMLHttpRequest();
+        xhr.onreadystatechange = function () {
+            console.log("State changed called . Ready state: " + xhr.readyState + " State: " + xhr.status)
+            if (xhr.readyState === 4) {
+                if (xhr.status == 200 || xhr.status == 201) {
+                    resolve(xhr.responseText);
+                }
+                else {
+                    reject({
+                        status: xhr.status,
+                        statusText: xhr.statusText
+                    })
+                    console.log("XHR failed")
+                }
             }
         }
-    }
 
-    xhr.open(methodType, url, async);
-    if (data) {
-        console.log(JSON.stringify(data));
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(JSON.stringify(data));
-    }
-    else {
-        xhr.send();
-    }
+        xhr.open(methodType, url, async);
+        if (data) {
+            console.log(JSON.stringify(data));
+            xhr.setRequestHeader("Content-Type", "application/json");
+            xhr.send(JSON.stringify(data));
+        }
+        else {
+            xhr.send();
+        }
 
-    console.log(methodType + " request sent to the server");
+        console.log(methodType + " request sent to the server");
+    });
+
 }
-
-const getURL = "http://localhost:3000/employee";
-function getUserDetails(data) {
-    console.log("Get user Data : " + data)
-}
-makeAjaxCall("GET", getURL, getUserDetails, true)
-
-const deleteURL = "http://localhost:3000/employee/1604589731061";
-function userDeleted(data) {
-    console.log("user deleted: " + data)
-}
-makeAjaxCall("DELETE", deleteURL, userDeleted, false);
-
-const postURL = "http://localhost:3000/employee";
-const empData = {
-    "name": "test",
-    "gender": "male",
-    "departMent": [
-      "HR"
-    ],
-    "salary": "30000",
-    "startDate": "1 Jan 2020",
-    "notes": "",
-    "id": 1604589731061,
-    "profileUrl": "../assets/profile-images/Ellipse -3.png"
-  }
-
-function userAdded(data) {
-    console.log("user Added: " + data)
-}
-makeAjaxCall("POST", postURL, userAdded, true, empData);
